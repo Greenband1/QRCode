eee8caeb6f973e5bdbcce4584227c17d2f3d7765
Robert Davis <greenband@gmail.com>
2024-10-23 11:01:36 -0500

    Update Squid analytics implementation to add snippet to main QRcode file

--- src/app/components/QRCodeGenerator.jsx
+++ src/app/components/QRCodeGenerator.jsx
@@ -24,6 +24,15 @@ const QRCodeGenerator = () => {
 
   useEffect(() => {
     setMounted(true);
+    // Add Squid analytics
+    (function(squid){
+      (window.$quid) || (window.$quid = {});
+      document.head.appendChild((function(s){ 
+        s.src='https://app.asksquid.ai/tfs/'+squid+'/sdk';
+        s.async=1; 
+        return s; 
+      })(document.createElement('script')));
+    })('6719103019d43af7a47a0bbb');
   }, []);
 
   const validateInputs = () => {
